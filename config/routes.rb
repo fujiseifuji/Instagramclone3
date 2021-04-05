5b06cdf2a145ca04c2d7ec874212e4d95ce3466a
fuji <fujifujiseisei@gmail.com>
2021-04-05 21:08:01 +0900

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  devise_for :users
   root 'static_pages#home'
   get  '/terms',   to: 'static_pages#terms'
-  get  '/signup',  to: 'users#new'
+  get  '/users/sign_up',  to: 'users#new'
+  resources :users
 end
